b5fb1b44328fc2678ce62f01c40e11477d9fb1d7
Anna Koutsoni <130784477+annakts@users.noreply.github.com>
2025-10-23 23:30:07 +0300

    Add files via upload

---
+++ LAB4/ask1s4.m
@@ -0,0 +1,54 @@
+#Ανάλυση και Σχεδιασμός τηλεφωνικού κέντρου
+clc;
+clear all;
+close all;
+pkg load queueing;
+
+% r = lambda / mu
+% c: number of servers 
+
+function p = erlangb_factorial (r,c)
+  s = 0;
+  for k = 0:1:c
+    s = s + (power(r,k)/factorial(k));
+  endfor
+  p = (power(r,c)/factorial(c))/s;
+endfunction
+
+
+function p = erlangb_iterative (r,c)
+  p = 1;
+  for i=0:1:c
+    p = ((r*p)/((r*p)+i));
+  endfor
+endfunction
+
+display("erlangb_factorial(9,9) =");
+disp(erlangb_factorial(9,9));
+
+display("erlangb(9,9) =");
+disp(erlangb(9,9));
+
+display("erlangb_iterative(9,9) =");
+disp(erlangb_iterative(9,9));
+
+display("erlangb_factorial(1024,1024) =");
+disp(erlangb_factorial(1024,1024));
+
+display("erlangb_iterative(1024,1024) =");
+disp(erlangb_iterative(1024,1024));
+
+P = zeros(0,200);
+
+for i = 1:1:200
+  P(i) = erlangb_iterative (i*(23/60),i)
+endfor
+
+
+
+
+figure(1);
+stem(P,'b',"linewidth",0.4);
+title("Blocking Probabilities")
+xlabel("servers");
+ylabel("blocking probability");
---
+++ LAB4/ask2s4.m
@@ -0,0 +1,85 @@
+#Σύστημα εξυπηρέτησης με δύο ανόμοιους εξυπηρετητές
+clc;
+clear all;
+close all;
+pkg load queueing;
+
+lambda = 1;
+m1 = 0.8;
+m2 = 0.4;
+
+threshold_0 = lambda/1;
+threshold_1a = lambda/(lambda+m1);
+threshold_1b = lambda/(lambda+m2);
+threshold_2_first = lambda/(lambda+m2+m1);
+threshold_2_second = (m1+lambda)/(lambda+m2+m1);
+
+current_state = 0; # what is my state now?
+arrivals = zeros(1,4); # arrivals in each state of the system
+total_arrivals = 0; # The total number of client arrivals in the system
+maximum_state_capacity = 2; # maximum number of clients in the system
+previous_mean_delay = 0; # estimated mean delay in the previous time interval
+delay_counter = 0;
+time = 0;
+
+while 1 > 0
+  time = time + 1;
+  
+  if mod(time,1000) == 0
+    for i=1:1:4
+      P(i) = arrivals(i)/total_arrivals;
+    endfor
+    
+    delay_counter = delay_counter + 1;
+
+    mean_delay = 0*P(1) + 1*P(2) + 1*P(3) + 2*P(4);   
+    
+    delay_table(delay_counter) = mean_delay;
+    
+    if abs(mean_delay - previous_mean_delay) < 0.00001
+       break;
+    endif
+    previous_mean_delay = mean_delay;
+  endif
+  
+  random_number = rand(1);
+  
+  if current_state == 0
+    if random_number < threshold_0
+      current_state = 1;
+      arrivals(1) = arrivals(1) + 1;
+      total_arrivals = total_arrivals + 1;
+    endif
+  elseif current_state == 1
+    if random_number < threshold_1a
+      current_state = 3;
+      arrivals(2) = arrivals(2) + 1;
+      total_arrivals = total_arrivals + 1;
+    else
+      current_state = 0;
+    endif
+  elseif current_state == 2
+    if random_number < threshold_1b
+      current_state = 3;
+      arrivals(3) = arrivals(3) + 1;
+      total_arrivals = total_arrivals + 1;
+    else
+      current_state = 0;
+    endif
+  else
+      if random_number < threshold_2_first
+        arrivals(4) = arrivals(4) + 1;
+        total_arrivals = total_arrivals + 1;
+      elseif random_number < threshold_2_second
+        current_state = 2;
+      else
+        current_state = 1;
+      endif
+   endif
+  
+endwhile
+
+display(P(1));
+display(P(2));
+display(P(3));
+display(P(4));
